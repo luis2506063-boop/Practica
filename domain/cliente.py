c8b5d0d676798e938a21a9e137a633b3dbb92643
Luis Marin <luismiguelmarinovando@gmail.com>
2026-03-25 22:31:06 -0500

    Cree la clase clientes junto con su respectivos métodos

---
+++ domain/cliente.py
@@ -0,0 +1,29 @@
+class cliente:
+    def __init__(self, nombre:str, apellido:str):
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def obtener_descuento(self, total:float) -> float:
+        return 0.0 #El cliente normal no ahorra nada
+
+
+class ClienteVip(cliente):
+    def __init__(self, nombre:str, apellido:str):
+        super().__init__(nombre, apellido)
+
+    def obtener_descuento(self, total:float) -> float:
+        #Aplicamos el 10% de descuento
+        descuento = total * 0.1
+        return descuento
+
+
+class ClienteBlack(cliente):
+    def __init__(self, nombre:str, apellido:str):
+        super().__init__(nombre, apellido)
+
+    def obtener_descuento(self, total:float) -> float:
+        #Eñ cliente Black ahorra un 30%
+        descuento = total * 0.3
+        return descuento
+
+
